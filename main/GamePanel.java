69f13467d26aae0eba9746b01cbb02fd70596ab8
RPedreira <rnpedreira4@gmail.com>
2023-05-15 23:32:40 -0300

    EP3

---
+++ main/GamePanel.java
@@ -0,0 +1,66 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
+import javax.swing.JPanel;
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
+public class GamePanel extends JPanel {
+	private MouseInputs mouseInputs;
+	private float xDelta = 100f, yDelta = 100f;
+	private float xDir = 1f, yDir = 1f;
+	private Color color;
+	private Random random;
+	
+	public GamePanel() {
+		random = new Random();
+		mouseInputs = new MouseInputs(this);
+		addKeyListener(new KeyboardInputs(this));
+		addMouseListener(mouseInputs);
+		addMouseMotionListener(mouseInputs);
+	}
+	
+	public void changeXDelta (int value) {
+		this.xDelta += value;
+	}
+	
+	public void changeYDelta (int value) {
+		this.yDelta += value;
+	}
+	
+	public void setRectPosition(int x, int y) {
+		this.xDelta = x;
+		this.yDelta = y;
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		updateRect();
+		g.setColor(color);
+		g.fillRect((int)xDelta, (int)yDelta, 200, 50);
+	}
+
+	private void updateRect() {
+		xDelta += xDir;
+		if(xDelta > 400 || xDelta < 0) {
+			xDir *= -1;
+			color = getRandomColor();
+		}
+		yDelta += yDir;
+		if(yDelta > 400 || yDelta < 0) {
+			yDir *= -1;
+			color = getRandomColor();
+		}
+		
+	}
+
+	private Color getRandomColor() {
+		int r = random.nextInt(256);
+		int g = random.nextInt(256);
+		int b = random.nextInt(256);
+		
+		return new Color(r, g, b);
+	}
+}
