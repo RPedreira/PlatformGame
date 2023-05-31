f31398e56c7df701c055e3b831e652421502ecff
RPedreira <rnpedreira4@gmail.com>
2023-05-31 16:01:43 -0300

    LevelCreation

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,26 +1,53 @@
 package main;
 
+import java.awt.Graphics;
+
+import entities.Player;
+import levels.LevelManager;
+
 public class Game implements Runnable {
 	private GameWindow gameWindow;
 	private GamePanel gamePanel;
 	private Thread gameThread;
 	private final int FPS_SET = 120;
 	private final int UPS_SET = 200;
+	private Player player;
+	private LevelManager levelManager;
+	
+	public static final int TILES_DEFAULT_SIZE = 32;
+	public static final float SCALE = 1.5f;
+	public static final int TILES_IN_WIDTH = 26;
+	public static final int TILES_IN_HEIGHT = 14;
+	public static final int TILES_SIZE = (int)(TILES_DEFAULT_SIZE * SCALE);
+	public static final int GAME_WIDTH = TILES_SIZE * TILES_IN_WIDTH;
+	public static final int GAME_HEIGHT = TILES_SIZE * TILES_IN_HEIGHT;
 	
 	public Game() {
-		gamePanel = new GamePanel();
+		initClasses();
+		gamePanel = new GamePanel(this);
 		gameWindow = new GameWindow(gamePanel);
 		gamePanel.requestFocus(true);
 		startGameLoop();
 	}
 	
+	private void initClasses() {
+		player = new Player(200,200);
+		levelManager = new LevelManager(this);
+	}
+
 	private void startGameLoop() {
 		gameThread = new Thread(this);
 		gameThread.start();
 	}
 	
 	public void update() {
-		gamePanel.updateGame();
+		player.update();
+		levelManager.update();
+	}
+	
+	public void render(Graphics g) {
+		levelManager.draw(g);
+		player.render(g);
 	}
 	
 	@Override
@@ -56,5 +83,13 @@ public class Game implements Runnable {
 			}
 		}
 		
+	}
+	
+	public void windowFocusLost() {
+		player.resetDirBooleans();
+	}
+	
+	public Player getPlayer() {
+		return player;
 	}
 }
---
+++ src/utilz/LoadSave.java
@@ -0,0 +1,45 @@
+package utilz;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+import java.awt.Color;
+import main.Game;
+
+public class LoadSave {
+	public static final String PLAYER_ATLAS = "knight_merged_2.png";
+	public static final String LEVEL_ATLAS = "Front_tiles_merged.png";
+	public static final String LEVEL_ONE_DATA = "LEVEL_ONE_DATA.png";
+	
+	public static BufferedImage GetSpriteAtlas(String fileName) {
+		BufferedImage img = null;
+		InputStream is = LoadSave.class.getResourceAsStream("/" + fileName);
+		try {
+			img = ImageIO.read(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e){
+				e.printStackTrace();
+			}
+		}
+		return img;
+	}
+	
+	public static int[][] GetLevelData(){
+		int[][] levelData = new int[Game.TILES_IN_HEIGHT][Game.TILES_IN_WIDTH];
+		BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
+		for(int i = 0; i < img.getHeight(); i++)
+			for(int j = 0; j < img.getWidth(); j++) {
+				Color color = new Color(img.getRGB(j, i));
+				int value = color.getRed();
+				if(value > 64)
+					value = 63;
+				levelData[i][j] = value; 
+			}
+		return levelData;
+	}
+}
